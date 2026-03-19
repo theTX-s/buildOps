558419ee68380c6de63ae2ed2c7d49d349664f0f
thetx <99096693+thetx@users.noreply.github.com>
2026-03-19 20:20:17 +0530

    Added timeout and retry limits fordb connections

--- backend/BuildOps.API/Program.cs
+++ backend/BuildOps.API/Program.cs
@@ -6,7 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString, npgsqlOptions =>
+{
+    npgsqlOptions.EnableRetryOnFailure(maxRetryCount = 3, maxRetryDelay = 5);
+    npgsqlOptions.CommandTimeout(30);
+}).UseSnakeCaseNamingConvention());
 
 builder.Services.AddControllers();
 
