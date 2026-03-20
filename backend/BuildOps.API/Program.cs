52ce384ce1679257fb76f7ecc501f76037c52bb3
thetx <99096693+thetx@users.noreply.github.com>
2026-03-20 21:14:04 +0530

    Added DB Context (#6)
    * Added DB Context
    
    * Added timeout and retry limits for db connections

--- backend/BuildOps.API/Program.cs
+++ backend/BuildOps.API/Program.cs
@@ -1,6 +1,16 @@
+using BuildOps.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString, npgsqlOptions =>
+{
+    npgsqlOptions.EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(5), errorCodesToAdd: null);
+    npgsqlOptions.CommandTimeout(30);
+}).UseSnakeCaseNamingConvention());
 
 builder.Services.AddControllers();
 
